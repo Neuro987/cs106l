9624cca5206b35a50b9d0e9ab8094924d9176d85
Neuro987 <changxiaoyang01@gmail.com>
2025-05-07 21:05:53 +0900

    test commit

--- assignment1/main.cpp
+++ assignment1/main.cpp
@@ -25,9 +25,9 @@ const std::string COURSES_NOT_OFFERED_PATH = "student_output/courses_not_offered
  * Hint: Remember what types C++ streams work with?!
  */
 struct Course {
-  /* STUDENT TODO */ title;
-  /* STUDENT TODO */ number_of_units;
-  /* STUDENT TODO */ quarter;
+  std::string title;
+  int number_of_units;
+  std::string quarter;
 };
 
 /**
